f21c0bbbf2e2477084a4fbe92b7573bc8d6ec0e8
Aqurds <omarf1320@gmail.com>
2019-02-11 20:34:23 +0600

    blog delete/update button added

--- script.py
+++ script.py
@@ -317,5 +317,36 @@ def admin():
     return render_template('account.html')
 
 
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('blogsingle', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('new-post.html', form=form, post=post)
+
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('admin'))
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
